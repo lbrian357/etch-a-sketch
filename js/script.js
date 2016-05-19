476276d4a1851e2e2ad3bae8ba92031fb93937b9
lbrian357 <lbrian357@gmail.com>
2016-05-19 17:31:37 +0100

    sketch with the option to change grid numbers

--- js/script.js
+++ js/script.js
@@ -5,30 +5,39 @@ console.log('hello world js')
 
 /*creates a table*/
 var colrow = 16;
-$(document).ready(function(){
+
+$(document).ready(grid =function(){
 	$('body').append('<table></table>');
 	/*create 16 rows in the table*/
 		var row = '<tr></tr>'
-		for (var i = 0; i<16; i++){
+		for (var i = 0; i<colrow; i++){
 			$('table').append(row);
 		};
 		/*creates 16 columns in the table*/
 		var col = '<td></td>'
-		for (var i = 0; i<16; i++){
+		for (var i = 0; i<colrow; i++){
 			$('tr').append(col);
 		};
 		/*puts a div in each td of table*/
 		$('td').append('<div></div>');
 		/*makes div fit inside 800px*/
-		var dimension = 800/colrow;
+		var dimension = 600/colrow;
 		$('div').css('width', dimension+'px');
 		$('div').css('height', dimension+'px');
 		/*sets hover over to be black*/
 		$('div').hover(function(){
 			$(this).css('background-color', 'black');
 		})
+		/*adds button to create another grid*/
+		$('table').before('<button type="button">Click me to create another grid within 600px</button><br>');
+		$('button').click(function(){
+			colrow = prompt('enter how many rows and columns you want for the new grid');
+			$('body').empty();
+			$('body').append(grid);
+		})
 
 });
+
 
 
 
